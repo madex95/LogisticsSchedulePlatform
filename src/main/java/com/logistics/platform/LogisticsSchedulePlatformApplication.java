e49fcb9815441de7e94e3d9bdd5c4c196716bf8f
madex95 <madex8534@gmail.com>
2023-08-31 15:53:15 +0900

    2023.08.31 1. init commit and push 2. 부트스트랩 적용 완료 3. properties 기본 세팅 완료 (DB, 로깅, 포트, 메퍼경로)

---
+++ src/main/java/com/logistics/platform/LogisticsSchedulePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.logistics.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsSchedulePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsSchedulePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logistics/platform/LogisticsSchedulePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistics.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsSchedulePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
